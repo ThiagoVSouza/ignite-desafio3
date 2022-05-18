98614beffaad756ecb1e30e5ea1fcfecddfebef9
ThiagoVSouzam <thiago@nortrix.com>
2022-05-18 12:01:28 -0300

    done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,16 +22,53 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    
+    // return sumAmount[product.id] = 1;
+
+    // return 
+    const newSumAmmount = {...sumAmount};
+
+    newSumAmmount[product.id] = product.amount;
+
+    return newSumAmmount;  
+    
+  }, {} as CartItemsAmount)
+
+  console.log(cartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      
+        /// aqui no axios eu poderia passar assim:
+        /// api.get<Product[]>() pra indicar que o tipo do elem é Product
+        /// dentro do map eu nao precisaria por o tipo e ele já identificaria oque é 
+
+        await api.get('products')
+        .then(
+            
+            response=>{
+                
+                    // console.log(response.data);
+                    // setTransactions(response.data.transactions);
+                    const formatado = response.data.map( ( elem : Product ) => ({
+
+                      ...elem,
+                      priceFormatted: formatPrice(elem.price)
+                      
+                    } )
+                    );
+
+                    // console.log("formatado:",formatado);
+
+                    setProducts(formatado);
+                        
+                });
+    
     }
 
     loadProducts();
@@ -39,29 +76,50 @@ const Home = (): JSX.Element => {
 
   function handleAddProduct(id: number) {
     // TODO
+
+    addProduct(id);
+
   }
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+      {
+
+        products.map( product => (
+
+                <li key={product.id}>
+                <img src={product.image} alt={product.title} />
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+                <button
+                  type="button"
+                  data-testid="add-product-button"
+                  onClick={() => handleAddProduct(product.id)}
+                >
+                  <div data-testid="cart-product-quantity">
+                    <MdAddShoppingCart size={16} color="#FFF" />
+                    {cartItemsAmount[product.id] || 0}
+                  </div>
+        
+                  <span>ADICIONAR AO CARRINHO</span>
+                </button>
+              </li>
+
+
+          )
+
+          )
+
+      }
+      
+
+      
+    
+    
     </ProductList>
   );
 };
 
 export default Home;
+
